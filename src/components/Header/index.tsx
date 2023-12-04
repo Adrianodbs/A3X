1ecfe580c2364d0c765c4b638ca51a70ac846efd
Adriano <adriano_alves_2008@hotmail.com>
2023-12-04 20:14:15 -0300

    Feat: create header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,21 @@
 export default function Header() {
-  return <div className="bg-slate-600">Header</div>
+  return (
+    <div className="flex justify-between items-center py-6 px-14 ">
+      <h1 className="text-green-300 text-[28px]">A3X</h1>
+
+      <ul className="flex justify-center items-center gap-6 cursor-pointer">
+        <li className="text-gray-400 hover:text-gray-900 transition duration-300">
+          Home
+        </li>
+
+        <li className="text-gray-400 hover:text-gray-900 transition duration-300">
+          Quem somos
+        </li>
+
+        <button className="bg-green-300 rounded-md p-2 font-semibold text-white-50 hover:bg-white-50 hover:text-green-300 transition duration-300">
+          Solicitar orçamento
+        </button>
+      </ul>
+    </div>
+  )
 }
