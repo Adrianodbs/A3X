14d2256eff8711d55b1715894b1e9f894cebe1f3
Adriano <adriano_alves_2008@hotmail.com>
2023-12-26 11:22:57 -0300

    Fix: responsive navbar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,27 +2,27 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div className="flex justify-between items-center py-6 px-14 ">
-      <h1 className="text-green-300 text-[28px]">A3X</h1>
+    <div className="flex flex-col md:flex-row justify-between items-center py-6 px-14 ">
+      <h1 className="text-green-500 text-[28px] ">A3X</h1>
 
-      <ul className="flex justify-center items-center gap-6 cursor-pointer">
+      <ul className="md:static relative md:w-fit top-[20%] w-full flex justify-center md:flex-row flex-col items-center gap-6 cursor-pointer">
         <Link
           to="/"
           className="text-gray-400 hover:text-gray-900 transition duration-300"
         >
-          <li>Home</li>
+          Home
         </Link>
         <Link
           to="/about"
           className="text-gray-400 hover:text-gray-900 transition duration-300"
         >
-          <li>Quem somos</li>
+          Quem somos
         </Link>
         <Link
           to="/budget"
           className="bg-green-300 rounded-md p-2 font-semibold text-white-50 hover:bg-white-50 hover:text-green-300 transition duration-300"
         >
-          <li>Solicitar orçamento</li>
+          Solicitar orçamento
         </Link>
       </ul>
     </div>
